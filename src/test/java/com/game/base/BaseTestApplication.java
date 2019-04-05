c245debba8d346c92e8a4399cfba9928e09bd37b
swathi-harwalkar <shareasmile79@gmail.com>
2019-04-05 23:03:02 +0300

    separated methods from test,implemented decrementbalance and verify page Elements

--- src/test/java/com/game/base/BaseTestApplication.java
+++ src/test/java/com/game/base/BaseTestApplication.java
@@ -15,117 +15,123 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
-
-
 public class BaseTestApplication {
-	
-	//initialising webdriver, testdata read etc
-	
+
+	// initialising webdriver, testdata read etc
+
 	public static WebDriver driver;
 	public static Properties Config = new Properties();
 	public static Properties OR = new Properties();
 	public static FileInputStream fis;
 
 	@BeforeSuite
-	public void setUp(){
-		
-		if(driver==null){
-		
+	public void setUp() {
+
+		if (driver == null) {
+
 			try {
-				fis = new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/properties/OR.properties");
+				fis = new FileInputStream(System.getProperty("user.dir")
+						+ "/src/test/resources/properties/OR.properties");
 				OR.load(fis);
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			
-				
+
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 			try {
-				fis = new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/properties/Config.properties");
+				fis = new FileInputStream(System.getProperty("user.dir")
+						+ "/src/test/resources/properties/Config.properties");
 				Config.load(fis);
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			
+
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			System.setProperty("webdriver.chrome.driver","C:\\Users\\swathi\\workspace\\SlotGame\\SlotGame\\src\\test\\resources\\executables\\chromedriver.exe");
+
+			System.setProperty(
+					"webdriver.chrome.driver",
+					"C:\\Users\\swathi\\workspace\\SlotGame\\SlotGame\\src\\test\\resources\\executables\\chromedriver.exe");
 			driver = new ChromeDriver();
-			//driver.get(Config.getProperty("testURL"));
+			// driver.get(Config.getProperty("testURL"));
 			driver.get("C:/Users/swathi/Test/Test_Task.html");
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-			
-		
-			
-			
+
 		}
-		
+
 	}
-	//@AfterSuite
-	public void tearDown(){
-		
-		if(driver!=null){
+
+	// @AfterSuite
+	public void tearDown() {
+
+		if (driver != null) {
 			driver.quit();
 		}
-		
+
 	}
-	
-	public int[][] slotNumbers(){
-		
-		int[][] numbers= new int[3][5];
-		
+
+	public int[][] slotNumbers() {
+
+		int[][] numbers = new int[3][5];
+
 		int i;
 		int j;
-		
-		try{
-		 for(i=0;i<3;i++){
-			 int I = i+1;
-			 for(j=0;j<5;j++){
-				 int J = j+1;
-				 numbers[i][j]=Integer.parseInt(driver.findElement(By.xpath("//*[@id='reel"+J+"']/div["+I+"]")).getText());
-				 
-				 
-			 }
-		 }
-		}catch(NumberFormatException nfe){
-			System.out.println("Error: Slot has empty or non-numeric values");
-		}
-		 
-		//To Print all numbers in slot
-		 /*for(i=0;i<3;i++){
-			 for(j=0;j<5;j++){
-				 System.out.print(numbers[i][j]);
-				 System.out.print(" ");
-			 }
-			 System.out.println("");
-			 
-		 }*/
-		 
-		 return numbers;
 
-		}
-	
-	
+		try {
+			for (i = 0; i < 3; i++) {
+				int I = i + 1;
+				for (j = 0; j < 5; j++) {
+					int J = j + 1;
+					numbers[i][j] = Integer.parseInt(driver
+							.findElement(
+									By.xpath("//*[@id='reel" + J + "']/div["
+											+ I + "]")).getText());
 
-	public static int captureCurrentBalance(){
-		//int balance = Integer.parseInt(driver.findElement(By.xpath(OR.getProperty("balanceTextBox"))).getText());
-		int balance = Integer.parseInt(driver.findElement(By.id(OR.getProperty("balanceTextBox"))).getAttribute("value"));
+				}
+			}
+		} catch (NumberFormatException nfe) {
+			
+			Assert.fail("Error: Slot has empty or non-numeric values");
+		}
+
+		// To Print all numbers in slot
+		/*
+		 * for(i=0;i<3;i++){ for(j=0;j<5;j++){ System.out.print(numbers[i][j]);
+		 * System.out.print(" "); } System.out.println("");
+		 * 
+		 * }
+		 */
+
+		return numbers;
+
+	}
+
+	public static int captureCurrentBalance() {
+
+		int balance = Integer.parseInt(driver.findElement(
+				By.id(OR.getProperty("balanceTextBox"))).getAttribute("value"));
 		return balance;
-	}	
-		
-		
-		  
+	}
+
+	public void spin() {
+		driver.findElement(By.id("spinButton")).click();
 	}
 	
+	public static boolean isWin(){
+		
+		return driver.findElement(By.id("winbox")).isDisplayed();
+		
+	}
 
+}
--- src/test/java/com/game/testcases/RunWithSetTestData.java
+++ src/test/java/com/game/testcases/RunWithSetTestData.java
@@ -20,114 +20,122 @@ import org.testng.annotations.Test;
 import com.game.base.BaseTestApplication;
 import com.game.uitilities.DataRead;
 
-public class RunWithSetTestData extends BaseTestApplication{
-	
-	/*@BeforeTest
-	public void getTestData(){
-		DataRead reader = new DataRead();
-		List<String> lines = reader.readFile();
-	}
-	@BeforeTest
-	public void setProperties() throws IOException{
-		Properties element = new Properties();
-		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/SlotGame/src/test/resources/OR.properties");
-		element.load(fis);
-		
-		System.out.println(element.getProperty("dataInputTextBox"));
-	}*/
-	
-	/*@Test
-	public void setDataSpin() {
-		
-		String s = driver.findElement(By.xpath(OR.getProperty("reel1_notch2"))).getText();
-		Assert.assertEquals(s, "");
-		driver.findElement(By.xpath(OR.getProperty("spinButton"))).click();
-		String s1 = driver.findElement(By.xpath(OR.getProperty("reel1_notch2"))).getText();
-		Assert.assertEquals(s1, "");
-		
-	}
-	
+public class RunWithSetTestData extends BaseTestApplication {
 
-	
 	@Test
-	
-	public void clickUntilWin(){
-		while(!(driver.findElement(By.id("winbox")).isDisplayed())){
-			driver.findElement(By.id("spinButton")).click();
+	public void verifyPageElementPresence() {
+
+		Assert.assertTrue((driver.getTitle()).equals("Test Task"));
+		Assert.assertTrue(driver.findElement(By.id("slot")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.id("balance-value"))
+				.isDisplayed());
+		Assert.assertTrue(driver.findElement(By.id("testdata")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.id("paytable")).isDisplayed());
+
+	}
+
+	@Test
+	public void spinDecrementsBalance() {
+		int balance = captureCurrentBalance();
+		spin();
+		if (!isWin()) {
+			int balanceNew = captureCurrentBalance();
+			Assert.assertEquals(balanceNew, balance - 1);
+		} else
+			spinDecrementsBalance();
+
+	}
+
+	@Test
+	public void spinUpdatesSlotTable() {
+		// verifying slot table numbers are updated on clicking spin
+		spin();
+		int[][] slot1 = slotNumbers();
+		spin();
+		int[][] slot2 = slotNumbers();
+		// compare table displayed after spin is not equal to previous
+		// table
+		Assert.assertTrue(slot1 != slot2);
+
+	}
+
+	@Test
+	public void clickUntilWin() {
+
+		// capturing initial balance
+		int balance = captureCurrentBalance();
+
+		while (!(driver.findElement(By.id("winbox")).isDisplayed())) {
+			spin();
 		}
-		System.out.println(driver.findElement(By.id("winbox")).getText());
-	}*/
-	
-	@Test(dataProvider="paytable")
-	
-	public void setDataWin(String combination, int win){
-		
-		WebDriverWait wait = new WebDriverWait(driver,10);
-		
-		
+		// capturing balance after Win
+		int balanceNew = captureCurrentBalance();
+		// Test passes if balance has increased after win
+		Assert.assertTrue(balanceNew > balance);
+
+	}
+
+	@Test(dataProvider = "paytable")
+	public void setDataWin(String combination, int win) {
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
 		driver.findElement(By.id("testdata")).clear();
 		driver.findElement(By.id("testdata")).sendKeys(combination);
-		
-		int balance = Integer.parseInt(driver.findElement(By.id("balance-value")).getAttribute("value"));  
-		
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("winbox")));
-		
-		driver.findElement(By.id("spinButton")).click();
-		try{
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("winbox")));
-		}catch(TimeoutException te){
-			
-			Assert.fail("Win Message is not displayed for combination" +combination);
+
+		int balance = Integer.parseInt(driver.findElement(
+				By.id("balance-value")).getAttribute("value"));
+
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By
+				.id("winbox")));
+
+		spin();
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By
+					.id("winbox")));
+		} catch (TimeoutException te) {
+
+			Assert.fail("Win Message is not displayed for combination"
+					+ combination);
 		}
 		String winText = driver.findElement(By.id("winbox")).getText();
 		String[] split = winText.split("\\s+");
 		int winDisplay = Integer.parseInt(split[1]);
-		int balanceNew = Integer.parseInt(driver.findElement(By.id("balance-value")).getAttribute("value")); 
+		int balanceNew = Integer.parseInt(driver.findElement(
+				By.id("balance-value")).getAttribute("value"));
 
-		int excpectedBalance =(balance - 1) + win;
+		int excpectedBalance = (balance - 1) + win;
 		Assert.assertEquals(winDisplay, win);
-		
-		System.out.println("Win is +winDisplay +win +combination "+winDisplay +win +combination);
-			
-		Assert.assertEquals(balanceNew,excpectedBalance); 
-				System.out.println("Balance is +excpectedBalance +balanceNew +combination " +excpectedBalance +balanceNew +combination);
-			
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("winbox")));
 
-		
+		System.out.println("Win is +winDisplay +win +combination " + winDisplay
+				+ win + combination);
+
+		Assert.assertEquals(balanceNew, excpectedBalance);
+		System.out
+				.println("Balance is +excpectedBalance +balanceNew +combination "
+						+ excpectedBalance + balanceNew + combination);
+
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By
+				.id("winbox")));
+
 	}
-	
+
 	@DataProvider
-	Object[][] paytable(){
-		
-		return new Object[][]{
-				{"11100", 60},
-				{"11110", 80},
-				{"11111", 100},
-				
-				{"22200", 120},
-				{"22220", 160},
-				{"22222", 200},
-				
-				{"33300", 180},
-				{"33330", 240},
-				{"33333", 300},
-				
-				{"44400", 240},
-				{"44440", 320},
-				{"44444", 400},
-				
-				{"55500", 300},
-				{"55550", 400},
-				{"55555", 500}
-				
-				
-				
+	Object[][] paytable() {
+
+		return new Object[][] { { "11100", 60 }, { "11110", 80 },
+				{ "11111", 100 },
+
+				{ "22200", 120 }, { "22220", 160 }, { "22222", 200 },
+
+				{ "33300", 180 }, { "33330", 240 }, { "33333", 300 },
+
+				{ "44400", 240 }, { "44440", 320 }, { "44444", 400 },
+
+				{ "55500", 300 }, { "55550", 400 }, { "55555", 500 }
+
 		};
-		
+
 	}
-	
-	
-	
 
 }
